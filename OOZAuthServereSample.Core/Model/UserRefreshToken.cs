03b6ec972096d3621f75ea3eb9b5dcdde117c7a0
ooz <ooz@ziraatteknoloji.com>
2021-11-13 23:17:17 +0300

    added new models --ooz

---
+++ OOZAuthServereSample.Core/Model/UserRefreshToken.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace OOZAuthServereSample.Core.Model
+{
+    public class UserRefreshToken
+    {
+       public string UserId { get; set; }
+        public string Code { get; set; }
+        public DateTime Expiration { get; set; }
+
+    }
+}

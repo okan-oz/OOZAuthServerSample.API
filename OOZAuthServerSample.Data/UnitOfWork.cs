adfd22e0efacb54ac69c61abacb49ec8e07855f0
ooz <ooz@ziraatteknoloji.com>
2021-11-20 22:28:11 +0300

    updated

---
+++ OOZAuthServerSample.Data/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OOZAuthServereSample.Core.Service.UnitOfWork;
+
+namespace OOZAuthServerSample.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public UnitOfWork(AppDbContext appContext)
+        {
+            _context= appContext;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
